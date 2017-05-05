cee1fb9eaeba5b7abd6bc26382b0ccf1a7e9742b
Ovidiu Calbajos <ovidiu.calbajos@gmail.com>
2017-05-05 21:21:44 +0300

    depth of JSON and rescue invalid entries in JSON
    windows_partitions
    - check OS for depth value
    - rescue invalid entries when validating JSON

--- lib/facter/windows_partitions.rb
+++ lib/facter/windows_partitions.rb
@@ -1,10 +1,21 @@
 Facter.add('partitions') do
   confine :kernel => 'windows'
 
+  # Windows 2012 R2
+  if :kernelversion == '6.4.9600'
+    depth = '999'
+  # Windows 2016
+  elsif :kernelversion == '10.0.14393'
+    depth = '100'
+  # Default
+  else
+    depth = '999'
+  end
+  
   require_relative '../puppet_x/disk_facts/underscore.rb'
   require 'json'
   setcode do
-    partitions = JSON.parse(Facter::Core::Execution.exec("powershell.exe -Command \"Get-Partition | Select-Object * -ExcludeProperty CimClass,CimInstanceProperties,CimSystemProperties | ConvertTo-Json -Depth 999 -Compress\""))
+    partitions = JSON.parse(Facter::Core::Execution.exec("powershell.exe -Command \"Get-Partition | Select-Object * -ExcludeProperty CimClass,CimInstanceProperties,CimSystemProperties | ConvertTo-Json -Depth #{depth} -Compress\"")) rescue []
     partitions_renamed = []
 
     # Make sure that we can handle only one partition
