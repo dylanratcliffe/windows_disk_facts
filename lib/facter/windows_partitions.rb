59e2c42efea230ce0b58c0cf69e046ab50b92f4a
Ovidiu Calbajos <ovidiu.calbajos@gmail.com>
2017-06-09 09:42:17 +0300

    Changed depth to 100

--- lib/facter/windows_partitions.rb
+++ lib/facter/windows_partitions.rb
@@ -4,17 +4,8 @@ Facter.add('partitions') do
   require_relative '../puppet_x/disk_facts/underscore.rb'
   require 'json'
   setcode do
-    kernelmajversion = Facter.value('kernelmajversion')
-    next if kernelmajversion == '6.0' # Get-Partition is not available on Windows Server 2008
-    next if kernelmajversion == '6.1' # Get-Partition is not available on Windows Server 2008 R2
 
-    if kernelmajversion.split('.')[0].to_i >= 10 # If it's 2016 or newer we can no longer use depth > 100
-      depth = '100'
-    else
-      depth = '999'
-    end
-
-    partitions = JSON.parse(Facter::Core::Execution.exec("powershell.exe -Command \"Get-Partition | Select-Object * -ExcludeProperty CimClass,CimInstanceProperties,CimSystemProperties | ConvertTo-Json -Depth #{depth} -Compress\""))
+    partitions = JSON.parse(Facter::Core::Execution.exec("powershell.exe -Command \"Get-Partition | Select-Object * -ExcludeProperty CimClass,CimInstanceProperties,CimSystemProperties | ConvertTo-Json -Depth 100 -Compress\""))
     partitions_renamed = []
 
     # Make sure that we can handle only one partition
